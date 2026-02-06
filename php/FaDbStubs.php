e4976c110362ab12a3673a79025b2a0f6a357a67
Kevin Fraser <kevin@ksfraser.ca>
2026-02-05 19:38:05 -0700

    Add db_fetch_assoc() function for complete FA_ProductAttributes support
    - Add db_fetch_assoc() function to FaDbStubs.php
    - This function is required by FrontAccountingDbAdapter
    - Provides associative array result fetching for database queries

--- php/FaDbStubs.php
+++ php/FaDbStubs.php
@@ -108,3 +108,10 @@ namespace {
 		}
 	}
 }
+
+if (!function_exists('db_fetch_assoc')) {
+function db_fetch_assoc($res) {
+// Alias for db_fetch in FA - returns associative array
+return db_fetch($res);
+}
+}
